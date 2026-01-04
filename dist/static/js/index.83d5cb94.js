e3107d1ed4bf35b618fc8dcb5f142cc9a516be12
Manendra Singh Rawat <mannuatnri.itm@gmail.com>
2026-01-04 18:09:18 +0530

    uploaded dist

---
+++ dist/static/js/index.83d5cb94.js
@@ -0,0 +1,12 @@
+(()=>{"use strict";var t={};function e(s){var r=t[s];if(void 0!==r)return r.exports;var a=t[s]={exports:{}};return __webpack_modules__[s](a,a.exports,e),a.exports}e.p="/";let s=e.p+"static/image/logo.93a0f632.avif";document.getElementById("app").innerHTML=`
+  <div class="container">
+    <h1>Rsbuild JavaScript Project 🚀</h1>
+    
+    <p>Image support:</p>
+    <img src="${s}" class="logo" />
+
+    <div class="test1">Test</div>
+
+   
+  </div>
+`})();
